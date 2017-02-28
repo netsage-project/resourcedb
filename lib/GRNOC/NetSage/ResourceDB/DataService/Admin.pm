30b04965d0522fe67981d4031f12489ba9b4ba7b
Michael Johnson <mj82@grnoc.iu.edu>
2017-02-28 20:41:04 +0000

    Add Admin GWS and DS

---
+++ lib/GRNOC/NetSage/ResourceDB/DataService/Admin.pm
@@ -0,0 +1,116 @@
+package GRNOC::NetSage::ResourceDB::DataService::Admin;
+
+use strict;
+use warnings;
+
+use GRNOC::Config;
+use GRNOC::DatabaseQuery;
+
+use Data::Dumper;
+
+use base 'GRNOC::NetSage::ResourceDB::DataService';
+
+my $singleton;
+
+### constructor ###
+sub new {
+
+    my $caller = shift;
+
+    my $class = ref( $caller );
+    $class = $caller if ( !$class );
+
+    # if we've created this object (singleton) before, just return it
+    return $singleton if ( defined( $singleton ) );
+
+    my $self = $class->SUPER::new( @_ );
+
+    bless( $self, $class );
+
+    # store our newly created object as the singleton
+    $singleton = $self;
+
+    my $config = GRNOC::Config->new( config_file => $self->{'config_file'}, force_array => 0);
+    $self->{'config'} = $config;
+
+    return $self;
+}
+
+sub insert_ip_blocks {
+
+    my ( $self, %args ) = @_;
+
+    my $remote_user = $args{'remote_user'};
+
+    my $select_fields = [
+                         'ip_block.ip_block_id as ip_block_id',
+                         'ip_block.name as name',
+                         'ip_block.addr_str as addr_str',
+                         'ip_block.addr_lower as addr_lower',
+                         'ip_block.addr_upper as addr_upper',
+                         'ip_block.mask as mask',
+                         'ip_block.asn as asn',
+                         'ip_block.organization_id as organization_id',
+                         'ip_block.country_code as country_code',
+                         'ip_block.country_name as country_name',
+                         'ip_block.continent_code as continent_code',
+                         'ip_block.continent_name as continent_name',
+                         'ip_block.postal_code as postal_code',
+                         'ip_block.latitude as latitude',
+                         'ip_block.longitude as longitude',
+                         'ip_block.project_id as project_id',
+                         'ip_block.discipline_id as discipline_id',
+                         'ip_block.role_id as role_id',
+                         'role.name as role_name',
+                         'organization.name as organization_name',
+                         'project.name as project_name',
+                         'discipline.name as discipline_name',
+                         ];
+
+    my @where = ();
+
+    # handle optional ip_block_id param
+    my $role_id_param = GRNOC::MetaParameter->new( name => 'ip_block_id',
+                                                   field => 'ip_block.ip_block_id' );
+
+    @where = $role_id_param->process( args => \%args,
+                                      where => \@where );
+
+    # get the order_by value
+    my $order_by_param = GRNOC::MetaParameter::OrderBy->new();
+    my $order_by = $order_by_param->parse( %args );
+
+    my $limit = $args{'limit'};
+    my $offset = $args{'offset'};
+
+    my $from_sql = 'ip_block ';
+    $from_sql .= 'left join organization on ( ip_block.organization_id = organization.organization_id ) ';
+    $from_sql .= 'left join role on ( ip_block.role_id = role.role_id ) ';
+    $from_sql .= 'left join discipline on ( ip_block.discipline_id = discipline.discipline_id ) ';
+    $from_sql .= 'left join project on ( ip_block.project_id = project.project_id ) ';
+
+    my $results = $self->dbq_rw()->insert( table => $from_sql,
+                                           fields => $select_fields,
+                                           where => [-and => \@where],
+                                           order_by => $order_by,
+                                           limit => $limit,
+                                           offset => $offset );
+
+    if ( !$results ) {
+
+        $self->error( 'An unknown error occurred getting the ip blocks.' );
+        return;
+    }
+
+    my $num_rows = $self->dbq_rw()->num_rows();
+
+    my $result = GRNOC::NetSage::ResourceDB::DataService::Result->new( results => $results,
+                                                                 total => $num_rows,
+                                                                 offset => $offset );
+
+    return $result;
+
+}
+
+1;
+
---
+++ lib/GRNOC/NetSage/ResourceDB/GWS/Admin.pm
@@ -0,0 +1,143 @@
+package GRNOC::NetSage::ResourceDB::GWS::Admin
+
+use strict;
+use warnings;
+
+use GRNOC::Config;
+use GRNOC::DatabaseQuery;
+use GRNOC::WebService::Method;
+use GRNOC::WebService::Regex;
+use GRNOC::NetSage::ResourceDB::DataService::Admin;
+
+use Data::Dumper;
+
+our $websvc;
+
+use base 'GRNOC::NetSage::ResourceDB::GWS';
+
+sub new {
+
+    my $caller = shift;
+
+    my $class = ref( $caller );
+    $class = $caller if ( !$class );
+
+    my $self = $class->SUPER::new( @_ );
+
+    bless( $self, $class );
+
+
+    return $self;
+}
+
+sub _init_get_methods {
+
+    my $self = shift;
+    my $method_in = shift;
+    my $args = shift;
+
+    my $method;
+
+
+    $self->valid_dynamic_db_names( $self->user_ds()->valid_dynamic_db_names() );
+
+    $self->_init_dynamic_get_methods( @_ );
+
+
+    # get_ip_blocks
+    $method = GRNOC::WebService::Method->new( name => 'get_ip_blocks',
+                                                   description => "Returns the IP blocks.",
+                                                   expires => "-1d",
+                                                   #default_order_by => ['name'],
+                                                   callback => sub { $self->_get_ip_blocks( @_ ) } );
+
+    # add the optional 'ip_block_id' input param to the get_ip_blocks() method
+    $method->add_input_parameter( name        => 'ip_block_id',
+                                  pattern     => $INTEGER,
+                                  required    => 0,
+                                  multiple    => 1,
+                                  description => 'The id of the IP block');
+
+    $self->websvc()->register_method( $method );
+
+
+
+}
+
+sub _init_dynamic_get_methods {
+    my ( $self, $method_in, $args ) = @_;
+    #my $self = shift;
+    #my $method_in = shift;
+    #my $args = shift;
+
+    foreach my $name ( keys %{ $self->valid_dynamic_db_names() } ) {
+        my $method;
+        # get_roles
+        $method = GRNOC::WebService::Method->new( name => "get_${name}s",
+            description => "Returns the ${name}s",
+            expires => "-1d",
+            #default_order_by => ['name'],
+            callback => sub { $self->_get_table_dynamically( $name, $method_in, $args ) } );
+
+        # add the optional 'role_id' input param to the get_roles() method
+        $method->add_input_parameter( name        => "${name}_id",
+            pattern     => $INTEGER,
+            required    => 0,
+            multiple    => 1,
+            description => "The id of the $name");
+
+        $self->websvc()->register_method( $method );
+
+
+
+    }
+
+
+}
+
+
+### callbacks ###
+
+sub _get_table_dynamically {
+
+    my ( $self, $name, $method, $args ) = @_;
+
+    my $result = $self->user_ds()->get_table_dynamically( $name, $self->process_args( $args ) );
+
+    # handle error
+    if ( !$result ) {
+
+        $method->set_error( $self->user_ds()->error() );
+        return;
+    }
+
+    return {'results' => $result->results(),
+            'total' => $result->total(),
+            'offset' => $result->offset(),
+            'name' => $name,
+            'warning' => $result->warning()};
+}
+
+
+sub _get_ip_blocks {
+
+    my ( $self, $method, $args ) = @_;
+
+    my $result = $self->user_ds()->get_ip_blocks( $self->process_args( $args ) );
+
+    # handle error
+    if ( !$result ) {
+
+        $method->set_error( $self->user_ds()->error() );
+        return;
+    }
+
+    return {'results' => $result->results(),
+            'total' => $result->total(),
+            'offset' => $result->offset(),
+            'warning' => $result->warning()};
+}
+
+
+1;
+
