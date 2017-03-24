8bf1690cfaff0ce5693c1a733d3edfb12965a9b4
Michael Johnson <mj82@grnoc.iu.edu>
2017-03-24 18:54:32 +0000

    Add country/continent code parameters

--- lib/GRNOC/NetSage/ResourceDB/GWS/Admin.pm
+++ lib/GRNOC/NetSage/ResourceDB/GWS/Admin.pm
@@ -284,6 +284,15 @@ sub _add_dynamic_add_update_parameters {
                 required    => 0,
                 description => "The owner of the $name");
 
+        } elsif ( $field eq "country_code" || $field eq "continent_code" ) {
+            # add the optional 'url' input param to all the basic dynamic methods
+            $method->add_input_parameter(
+                name        => $field,
+                pattern     => $TEXT,
+                multiple    => 0,
+                required    => 0,
+                description => "The code of the $name");
+
         } elsif ( $field eq "url" ) {
             # add the optional 'url' input param to all the basic dynamic methods
             $method->add_input_parameter(
