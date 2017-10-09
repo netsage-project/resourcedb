70c4ed322592248fc4c4d885b7a6bfac91dba76a
Jonathan Stout <jonstout@grnoc.iu.edu>
2017-10-09 10:14:10 -0400

    adding user.js to imports

--- www/static/assets/js/config.js
+++ www/static/assets/js/config.js
@@ -1,3 +1,3 @@
-var mapboxAccessToken = '';
+var mapboxAccessToken = 'pk.eyJ1Ijoib3ZlcjkwMDAiLCJhIjoiY2owYTFxdzN3MGVwNDMzczQyMGx5NmpkdSJ9.tqyiSUSEvn4NSregR7tNfg';
 var basePath = '/';
 var baseUrl  = '/';
