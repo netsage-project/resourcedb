ae8aefad9736ef09a17e62ec6421919be441c6d4
Lisa Ensman <lensman@globalnoc.iu.edu>
2017-10-10 19:19:12 +0000

    Added an organization marker to organization maps

--- www/static/assets/js/gui/map.js
+++ www/static/assets/js/gui/map.js
@@ -3,10 +3,11 @@
 /**
  * renderMap draws a map in the html elment with id 'mapid'.
  *
- * @param resources - Array of objects with params latitude, longitude, and name
+ * @param resources - Array of objects with params latitude, longitude, and name (resources)
+ * @param [orgs] - Array of objects with params latitude, longitutde, and name (organizations)
  * @param [onRender] - Callback to be executed when the map has been drawn. Is passed the leaflet map object.
  */
-function renderMap(resources, onRender) {
+function renderMap(resources, orgs, onRender) {
     var map = L.map('mapid').setView([0, 0], 3);
     var url = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=' + mapboxAccessToken;
 
@@ -20,19 +21,37 @@ function renderMap(resources, onRender) {
         var latlng = L.latLng(resources[i].latitude, resources[i].longitude);
         latlngPoints.push(latlng);
 
-        var marker = L.marker([resources[i].latitude, resources[i].longitude]).addTo(map);
+        var marker = L.circleMarker([resources[i].latitude, resources[i].longitude]).addTo(map);
         marker.bindPopup("<b>" + resources[i].name + "</b><br/>");
+    }
+
+    if (orgs != undefined) {
+        for (var i = 0; i < orgs.length; i++) {
+            if ( orgs[i].latitude == 0 && orgs[i].longitude == 0 ) continue;
+            var latlng = L.latLng(orgs[i].latitude, orgs[i].longitude);
+            latlngPoints.push(latlng);
+
+            var marker = L.circleMarker([orgs[i].latitude, orgs[i].longitude],{color: '#ff4d4d'}).addTo(map);
+            marker.bindPopup("<b>" + orgs[i].name + "</b><br/>");
+        }
     }
 
     var bounds = L.latLngBounds(latlngPoints);
 
-    // Sets an arbitrary zoom level for a single point.
-    var zoom   = 5;
-    var center = [40, -90];
 
-    if (latlngPoints.length > 1) {
+    var center;
+    var zoom;
+    if (latlngPoints.length == 1) {
+        zoom   = 5;
+        center = [resources[0].latitude, resources[0].longitude];
+    }
+    else if (latlngPoints.length > 1) {
         zoom = map.getBoundsZoom(bounds);
         center = bounds.getCenter();
+    }
+    else {
+        zoom = 3;
+        center = [40, -50];
     }
 
     map.setView(center, zoom);
