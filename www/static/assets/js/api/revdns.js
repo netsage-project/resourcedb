a4fc7673c1a0acd0c04de33c2e83f7021c6526a8
Lisa Ensman <lensman@globalnoc.iu.edu>
2017-11-03 15:45:08 +0000

    Added revdns.js files

---
+++ www/static/assets/js/api/revdns.js
@@ -0,0 +1,25 @@
+// GlobalNOC 2017
+
+// Gets reverse DNS information for the CIDR address.
+function getReverseDNS(cidr, onSuccess) {
+    console.log ("in getReverseDNS");
+/*
+    onSuccess("8.8.8.8 google.comx");
+
+    var args = cidr.split("/");
+    var addr = args[0];
+
+    var url = 'http://api.hackertarget.com/reversedns';
+    url += '?q=' + addr;
+    fetch(url, {
+        method: 'get'
+    }).then(function(response) {
+
+            console.log(response);
+            onSuccess(response);
+
+    }).catch(function(err) {
+        console.log(err);
+    });
+*/
+}
