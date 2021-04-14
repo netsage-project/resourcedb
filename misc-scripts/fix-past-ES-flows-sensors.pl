b297cf3b72e62e04dd680969b4a0829eeafe774d
Lisa Ensman <lensman@globalnoc.iu.edu>
2021-04-14 15:09:43 +0000

    committed version of fix-past-ES-flows-sensors.pl that I just used

--- misc-scripts/fix-past-ES-flows-sensors.pl
+++ misc-scripts/fix-past-ES-flows-sensors.pl
@@ -7,12 +7,30 @@ use Search::Elasticsearch;
 use Getopt::Long;
 use Data::Dumper;
 
-# This script updates flows in ElasticSearch FOR GPN from the time the script is run, going back in time.
-# Will replce sensor "netsage.gpn.onenet.net" with "gpn-kc", and flow_type = "netflow" with "sflow".
-# RUN MANUALLY   
+# This script updates sensor names in ElasticSearch from the time the script is run, going back in time.
+# Will replce sensor names as specfied
+# RUN MANUALLY   eg,   $ perl fix-past-ES-flows-sensors.pl -u netsage_service -p xxx - es prod > fix-sensors-irnc.out1
+# TEST BY UNCOMMENTING THINGS BELOW IN #####'s and EXITING EARLY.
 
-# DIDN'T ACTUALLY USE THIS, BUT I THINK IT WOULD WORK
+#!!!! SET CORRECT INDEX NAMES HERE !!!!
+my $index_names = "om-netsage-sox-*";   
+print ("DOING INDICES:  $index_names \n\n");
 
+#!!! NOTE:
+# To allow writing to indices, run something like this in the kibana console:
+# PUT om-ns-netsage-2019.09*/_settings 
+# {
+#   "index.blocks.write" : null
+#  }
+
+#!!!! SPECIFY OLD AND NEW SENSOR NAMES HERE   AND   IN QUERY BELOW
+# What to change the sensor names to        (old   =>   new)
+    my $new_sensor_names = {"NEAAR New York sFlow" => "NEAAR NY-London", 
+                            "SANReN Netflow"       => "SANReN London to Johannesburg",
+                            "rtr.sidco.sox.net"    => "SoX Nashville Sidco sFlow",
+                            "rtr.365dc.sox.net"    => "SoX Nashville 365DC sFlow",
+                            "rtr.56mar.sox.net"    => "SoX Atlanta Netflow",
+                            "SingAREN SOE1 Singapore SFlow" => "SingAREN Singapore 1"};
 
 
 
@@ -28,6 +46,7 @@ if (!$esver or !$username or !$pw) {
             die("ERROR. On the command line, enter the elasticsearch username and password with -u, -p, \n ".
                    "       and '-es prod' to use production Elasticsearch, '-es dev' to use dev ES\n");
 }
+
 # elasticsearch url with username, pw, host
 my $es_url;
 if ($esver eq 'prod') {
@@ -39,17 +58,6 @@ if ($esver eq 'prod') {
 } else {
     die ("ERROR: Use '-es dev' for dev ES, '-es prod' to use production ES\n");
 }
-
-########## my $index_names = "om-ns-netsage-";   # first part of names
-my $index_names = "om-ns-gpn-";   # first part of names
-print "indices : $index_names \n";
-
-#########
-    # for localhost testing
-#   my $index_names = "om-ns-test-netsage-";   
-#   my $es_url = 'localhost:9200';  ######
-#   my $index_names = "test";  #######
-#########
 
 # connect to ES
 # (for debugging, to see the HTTP requests and responses, add to new:  
@@ -65,29 +73,31 @@ if (!$es) {
 # Get indices (gets just names with -h)
 my $response = $es->cat->indices (
     index => $index_names.'*',
-    h => "index" ); 
+    h => "index" 
+); 
 my @indices = split("\n",$response);
 
-# some index names start with "shrink-", some don't! There are aliases for the ones with shrink,
-# we need to use those to sort right.
-my @renamed_indices;
-foreach my $index (@indices) {
-    $index =~ s/shrink-//;
-   push(@renamed_indices, $index);
-}
-my @sorted_indices = reverse sort @renamed_indices;
-###print Dumper \@sorted_indices; exit;
+my @sorted_indices = reverse sort @indices;
 
+#########
+#print Dumper \@sorted_indices; 
+#exit;
+########
+
+# Loop over indices, going back in time
 my $total_docs = 0;
 my $total_docs_done = 0;
 foreach my $index (@sorted_indices) {
 
 ######################
-#   print $index."\n";
 #    # to skip some indices or quit when reaching some index:
-#    if ($index =~ /2020\.08\./) { print ("QUITTING AT index 2020.08. \n"); last; }
+#    if ($index =~ /-2021\./) { print " skipping "; next; }
+#    if ($index =~ /-2020\./) { print ("QUITTING AT index 2020.08. \n"); last; }
 #    if ($index =~ /copy/) { next; }
 ######################
+# print $index. " ";
+# next; 
+#################
 
     # bulk_helper for updates of this index - will use to do many updates in one request
     # ####  NOTE: THE CURRENT VERSION OF BULK_HELPER REQUIRES "type" BUT ES VER 7 DOESN'T SUPPORT IT
@@ -116,55 +126,87 @@ foreach my $index (@sorted_indices) {
         }
     );
 
+    print "\n\n".strftime('%F %T',localtime).
+        " : Docs done = $total_docs_done,  should be = $total_docs.  Starting index = $index, "; 
+
+# !!!! EDIT HERE
     # Get docs 
-    #   In the query, use "terms" since we want an exact match and there are >1 possible values (asn's)
+    #   In the query, use "terms" since we want an exact match and there are >1 possible values 
     #       'bool => { should {'  makes an 'OR'
     # scroll_helper will send requests for batches of 'size' docs, as required.
     my $scroll = $es->scroll_helper(
         index => $index,
-        size  => 1000,    # number of docs per batch 
+        size  => 3000,    # number of docs per batch 
         scroll => '10m',  # >= time to process a batch
         body => { 
             sort => '_doc', # to actually NOT sort
             query => {
-                "term" => { "meta.sensor_id.keyword" => "netsage.gpn.onenet.net" }
-            }
-        }
+                bool => { 
+                    should => [
+                      {term => { "meta.sensor_id" => "NEAAR New York sFlow" }},
+                      {term => { "meta.sensor_id" => "SANReN Netflow" }},
+                      {term => { "meta.sensor_id" => "rtr.sidco.sox.net" }},
+                      {term => { "meta.sensor_id" => "rtr.365dc.sox.net" }},
+                      {term => { "meta.sensor_id" => "rtr.56mar.sox.net" }},
+                      {term => { "meta.sensor_id" => "SingAREN SOE1 Singapore SFlow" }}
+                    ],
+                "minimum_should_match" => 1,
+                } 
+            } 
+        } 
     );
-  #              bool => { should => [
-  #                  {terms => { "meta.sensor_id.keyword" => "netsage.gpn.onenet.net" }},
-  #              ] } 
-  #          } 
-  #      } 
-  #  );
 
     my $scroll_total = $scroll->total;   # not sure why this is a hash with value and 'relation' => 'eq'!
     my $ndocs = $scroll_total->{'value'};
+    print "num docs = $ndocs \n";
     $total_docs += $ndocs;
-    print "\n".strftime('%F %T',localtime).
-        " : Docs done = $total_docs_done,  should be = $total_docs.  Starting index = $index,  num docs = $ndocs, \n";
-    STDOUT->autoflush(1); 
+    STDOUT->autoflush(1);
+
+########
+### just print no. of docs to update
+#   next; 
+#########
 
     my $n = 0;
-DOCLOOP:  while (my $doc = $scroll->next) {
+    while (my $doc = $scroll->next) {
         $n++;
-        if ($n % 1000 == 0) { print "."; STDOUT->autoflush(1); }   
+        if ($n % 5000 == 0) { print "."; STDOUT->autoflush(1); }   # just some .'s to show things are happening
 
         my $id = $doc->{'_id'};
-        my  $to_replace = {
-                "meta.sensor_id"          => "gpn-kc",
-                "meta.flow_type"          => "sflow"
-            };    
+        my $indx = $doc->{'_index'};
+       
+##        print "$n. fixing ID = $id   in $indx\n"; ######
+########
+##      # Don't process, just loop through
+##       $total_docs_done++; 
+##       next; 
+#########
 
-        # Add action to $bulk if there's anything to update (updates haven't been done already!).
+        # the sensor name in this doc
+        my $doc_sensor = $doc->{'_source'}->{'meta'}->{'sensor_id'};
+    
+        # what to update
+        my $to_replace;
+        my $new_name = $new_sensor_names->{$doc_sensor};
+        if (!$new_name) { die(" no new name for $doc_sensor \n"); }
+
+        $to_replace->{'meta'}->{'sensor_id'} = $new_name;
+
+        # TEST THIS:  You can replace a number of fields at once here, if needed, by having more $to_replace values
+        # eg, also $to_replace->{'meta'}->{'sensor_group'} = "Group X";
+
+        # Add action to $bulk 
         # The update query will be sent when max_count is reached. (can also call flush to do it manually)
-        my $to_do =  { "meta"    => $to_replace };
-
-#        $bulk->add_action( update => { id => $id,
-#                                       doc => $to_do,
-#                                       doc_as_upsert => "true" }
-#                         );
+        $bulk->add_action( update => { id => $id,
+                                       doc => $to_replace,
+                                       doc_as_upsert => "true" }
+                         );
         
+##########
+## for testing, do just 1 or a few per index
+#if($n == 10) { print "\n"; last; } 
+##########
+
     } # end doc
 
     # finish any updates left
