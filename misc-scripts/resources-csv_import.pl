39ce8cbdc56a9837e84f51dec8d6bdcc83b3d1d9
Lisa Ensman <lensman@globalnoc.iu.edu>
2022-05-31 14:47:29 +0000

    updated resources import script

--- misc-scripts/resources-csv_import.pl
+++ misc-scripts/resources-csv_import.pl
@@ -18,25 +18,31 @@ use Data::Dumper;
 # ! Check the IP blocks in the CSV file. The script will warn you if there is a string match to any of them in the registry already, 
 # ! but does NOT check for, eg, a /32 belonging to an existing /24. 
 # ! It will go ahead and enter it after printing a warning!
-# The script will skip resources where the resource name or abbr+org_id are already in the db.
+# The script will skip resources where the resource name is already in the db (so you can run the same csv again)
 # DO BEFORE RUNNING:  Check everything over carefully.
-#                     Enter all Organizations in the database
+#                     Enter all Organizations and Projects in the database
 #                     Specify the csv file on the command line with -i or edit default location below. 
 #                     Specify 'source' on the command line with -s using some unique descriptive text.
+#                     Run in test mode.
 #                     see command line options below
-# spreadsheet columns
-# A [0] = org name (changed to org_id for saving to db table)
-# B [1] = resource name
-# C [2] = resource abbr (required for checks)
-# D [3] = ip list
-# E [4] = asn
-# F [5] = resource url 
-# G [6] = description
-# H [7] = lat, long
-# I [8] = country (changed to country_code)
-# J [9] = discipline (changed to discipline_id)
-# K [10]= role (changed to role_id)
-# L [11]= notes ($source is appended)
+# spreadsheet is assumed to have 1 row of headers
+# spreadsheet columns:
+#  A [0] = notes (do NOT import)
+# *B [1] = ip list
+#  C [2] = ASN
+# *D [3] = organization name (changes to org_id)
+# *E [4] = organization abbreviation (required to make full resource name)
+#  F [5] = sub-org (optional)
+# *G [6] = resource base name (name without org or sub-org)
+#  H [7] = resource short name (optional)
+# *I [8] = discipline (changes to discipline_id)
+#  J [9] = resource description
+#  K [10] = resource url
+#  L [11] = resource's lat, long
+# *M [12] = country (changes to country_code)
+# *N [13] = role (changes to role_id)
+# *O [14] = project (changes to project_id)
+# * required
 #-----------------------------
 sub usage() {
   print "  USAGE: perl resources-csv_import.pl
@@ -140,6 +146,7 @@ my $orgs;
 my $countries;
 my $disciplines;
 my $roles;
+my $projects;
 my $headers = "yes";
 
 # Read CSV File  (this way of reading allows for fields containing embedded newlines)
@@ -161,43 +168,66 @@ if (!defined $values){
         next;
     }
 
+    # IP list - remove all spaces before saving to the db (this is important for make-mmdb.pl)
+    $values->[1] =~ s/\s//g;    
+    if (! $values->[1] or $values->[1] eq ""){
+        print "?-?-?-?-? NO IPs ????? Skipping.\n-------------\n";
+        next;
+    }
+    my $ip_list = $values->[1];
 
-    # get the org id (make sure org exists already). Exact match is required!
-    # Will die if not found.
-    my $org_name = $values->[0];
-    next if ($org_name eq '');
+    # Construct full resource name
+    my @parts;
+    #my $org_abbr = "";
+    push(@parts, $values->[4]) if ($values->[4] ne "");
+    #my $sub_org = ""; 
+    push(@parts, $values->[5]) if ($values->[5] ne "");
+    #my $res_base_name = "";
+    push(@parts, $values->[6]) if ($values->[6] ne "");;
+    my $full_res_name = join(" - ",@parts);
+    if (! $full_res_name) {
+        print "?-?-?-?-? NO RESOURCE NAME (org abbr, sub org, or base name) ????? Skipping.\n-------------\n";
+        next;
+    }
+
+    # Get the org id (make sure org exists already). Exact match is required!
+    # Will die in the sub if not found.
+    my $org_name = $values->[3];
     my $org_id = get_org_id($org_name);
-    $values->[0] = $org_id;
+    $values->[3] = $org_id;
 
-    # remove all spaces from the IP list before saving to the db (this is important for make-mmdb.pl)
-    $values->[3] =~ s/\s//g;    
-
-    # check if the resource name is already in the registry [if yes, skip this resource]
-    # or if the org + resource abbr is already in the registry [if yes, skip this resource]
-    # or if any of the ips and blocks are already in [if yes, warn user] (just string matching)
-    my $res_name = $values->[1];
-    my $res_abbr = $values->[2];
-    my $ip_list = $values->[3];
-    my $skip = do_checks ($res_name, $res_abbr, $org_id, $ip_list);         
+    # Check if the resource name is already in the registry [if yes, skip this resource]
+    # Check if any of the ips are already in the registry [if yes, just warn user] (This is just string matching!)
+    my $skip = do_checks ($full_res_name, $ip_list);         
     next if ($skip);
 
-    # get the country code. Exact match is required!
-    my $country_name = $values->[8];
+    # Get the country code. Exact match is required!
+    # Will die in the sub if not found.
+    my $country_name = $values->[12];
     my $country_code = get_country_code($country_name);
-    $values->[8] = $country_code;
+    $values->[12] = $country_code;
 
-    # get the discipline id. Exact match is required!
-    my $discipline_name = $values->[9];
+    # Get the discipline id. Exact match is required!
+    # Will die in the sub if not found.
+    my $discipline_name = $values->[8];
     my $discipline_id = get_discipline_id($discipline_name);
-    $values->[9] = $discipline_id;
+    $values->[8] = $discipline_id;
 
-    # get the role id. Exact match is required!
-    my $role_name = $values->[10];
+    # Get the role id. Exact match is required!
+    # Will die in the sub if not found.
+    my $role_name = $values->[13];
     my $role_id = get_role_id($role_name);
-    $values->[10] = $role_id;
+    $values->[13] = $role_id;
 
-    # Add Resource
-    add_resource($values);
+    # Get the project id. Exact match is required!
+    # Will die in the sub if not found.
+    my $project_name = $values->[14];
+    my $project_id = get_project_id($project_name);
+    $values->[14] = $project_id;
+
+    # Add Resource 
+    # (if testing, just print something instead)
+    add_resource($full_res_name, $values);
 }
 
 # make sure it's end of the file
@@ -312,60 +342,62 @@ sub get_role_id {
     return $id;
 }
 
-sub do_checks {
-    my $res_name = shift;
-    my $res_abbr = shift;
-    my $org_id = shift;
-    my $ip_list = shift;
+sub get_project_id {
+    my $project_name = shift;
 
-    # abbr can only be 50 chars
-    my $abbr_length = length $res_abbr;
-    if ($abbr_length > 50) {
-        print "TRUNCATING short name for '$res_name' to 50 chars!\n";
-        $res_abbr = substr($res_abbr, 0, 50);
+    if ($projects->{$project_name}) {
+        return $projects->{$project_name};
     }
 
-    # See if the resource name, or org+abbr, has been used already (eg, part of the csv file already imported)
+    my $found = $dbq->select(
+        table => 'project',
+        fields => [ 'name', 'project_id' ],
+        where => {'name' => $project_name}
+    );
+    if (!$found) {
+        die "Project query error: ".Dumper $dbq->get_error();
+    }
+    if (@$found == 0) {
+        print "ERROR: Project '".$project_name."' was not found in the Registry.\n";
+        exit if ($mode ne "test"); 
+    }
+    
+    my $id = $found->[0]->{'project_id'}; 
+    $projects->{$project_name} = $id;
+    return $id;
+}
+
+sub do_checks {
+    my $full_res_name = shift;
+    my $ip_list = shift;
+
+    # See if the resource name has been used already (eg, part of the csv file already imported)
     # If yes, just return 1 and skip this row in the csv file.
     my $found = $dbq->select(
         table => 'ip_block',
         fields => [ 'ip_block_id', 'name' ],
-        where => { 'name' => $res_name }
+        where => { 'name' => $full_res_name }
     );
     if (!$found) {
         die "Resource query error: ".Dumper $dbq->get_error();
     }
     if (@$found > 0) {
-        print "SKIPPING resource '$res_name'. This name is already in the Registry.\n";
+        print "SKIPPING resource '$full_res_name'. This name is already in the Registry.\n-------------\n";
         # to skip insert
         return 1;
     }
     
-    $found = $dbq->select(
-        table => 'ip_block',
-        fields => [ 'ip_block_id', 'name' ],
-        where => { 'organization_id' => $org_id, 'abbr' => $res_abbr }
-    );
-    if (!$found) {
-        die "Resource query2 error: ".Dumper $dbq->get_error();
-    }
-    if (@$found > 0) {
-        print "SKIPPING resource '$res_name'. The org+shortname is already in the Registry.\n";
-        # to skip insert
-        return 1;
-    }
-
     # Make sure all IPs are in CIDR notation (have a / followed by 2-3 digits). 
     # See if any of the CIDRs in the ip list are already in the registry (exact string match)
     # If yes, print out a warning, but continue.
     my @ips = split(',', $ip_list);
     foreach my $ip (@ips) {
-        if ( $ip !~ /.*\/\d{2,3}/ ) {
+        if ( $ip !~ /.*\/\d{2,3}$/ ) {
             print "ERROR: A '/xx' is missing in '$ip'\n";
             exit if ($mode ne "test"); 
         }
         if ( $ip =~ /.*\/.*\/.*/ ) {
-            print "ERROR: Looks like a missing comma between IPs : '$ip'\n";
+            print "ERROR: Looks like a typo or missing comma between IPs : '$ip'\n";
             exit if ($mode ne "test"); 
         }
         my $found = $dbq->select(
@@ -377,49 +409,60 @@ sub do_checks {
             die "IP query error: ".Dumper $dbq->get_error();
         }
         if (@$found > 0) {
-            print "WARNING: $ip of resource '$res_name' is already in the Registry! \n".
+            print "WARNING: $ip of resource '$full_res_name' is already in the Registry! \n".
                 "         See existing resource '".$found->[0]->{'name'}."'.\n".
-                "         Inserting the new resource with this IP anyway!\n";
+                "         Will insert the new resource with this IP anyway!\n";
         }
     } 
-    # to continue with insert
+    # return 0 to continue with insert
     return 0;
 }
 
 sub add_resource {
+    my $full_res_name = shift;
     my $values = shift;
 
-    my $org_id = $values->[0];
-    my $name = $values->[1];
-    my $abbr = substr($values->[2], 0, 50);
-    my $ips = $values->[3];
-    my $asn = $values->[4];
-    my $res_url = $values->[5];
-    my $desc = $values->[6];
-    my $location = $values->[7];
-    my $country_code = $values->[8];
-    my $discipline_id = $values->[9];
-    my $role_id = $values->[10];
-    if ($values->[11]) { $values->[11] = $values->[11]."   "; }
-    my $notes = $values->[11].$source; 
+    my $ips = $values->[1];
+    my $asn = $values->[2];
+    my $org_id = $values->[3];
+    my $res_abbr = substr($values->[7], 0, 50);
+    $res_abbr = undef if ($res_abbr eq "");  # so a null will be inserted (so it won't complain about not being unique)
+    my $discipline_id = $values->[8];
+    my $desc = $values->[9];
+    my $res_url = $values->[10];
+    my $location = $values->[11];
+    my $country_code = $values->[12];
+    my $role_id = $values->[13];
+    my $proj_id = $values->[14];
+    my $notes = $source; 
+
     # lat and long - keep only up to 4 decimal places
-    my ($lat, $long) = split(",", $location);
-    ($lat)  = $lat  =~ /(-?\d+\.\d{0,4})/; 
-    ($long) = $long =~ /(-?\d+\.\d{0,4})/; 
-    
-# if testing, just print
+    my ($lat, $long);
+    if ($location eq "") { 
+        $lat = undef; # so a null will be inserted (even with this, UI doesn't not show Project or Event on resource page !?!) 
+        $long = undef;
+    }
+    else {
+        ($lat, $long) = split(",", $location);
+        ($lat)  = $lat  =~ /(-?\d+\.\d{0,4})/; 
+        ($long) = $long =~ /(-?\d+\.\d{0,4})/; 
+   }
+ 
+# if testing, just print (skip warnings about concatenating unset values)
 if ($mode eq "test") {
     no warnings qw(uninitialized);    
-    print "---WOULD INSERT:  $name, ABBR: $abbr, IPs: $ips, ORG_ID: $org_id, COUNTRY_CODE: $country_code, DISC_ID: $discipline_id, ROLE_ID: $role_id\n";
+    print "WOULD INSERT:  $full_res_name, ABBR: $res_abbr, IPs: $ips, ASN: $asn, ORG_ID: $org_id, COUNTRY_CODE: $country_code, \n";
+    print "                DISCIPLINE_ID: $discipline_id, ROLE_ID: $role_id,  LAT: $lat, LONG: $long, PROJECT_ID: $proj_id, NOTES: $notes\n";
+    print "-------------\n";
 }
 # if mode=doit, insert
 elsif ($mode eq "doit") {
 
-    # insert into db
+    # Insert into db
     my $res_id = $dbq->insert(
         table => 'ip_block',
-        fields => { name => $name,
-                    abbr => $abbr,
+        fields => { name => $full_res_name,
+                    abbr => $res_abbr,
                     description => $desc,
                     addr_str => $ips,
                     asn => $asn,
@@ -435,12 +478,12 @@ elsif ($mode eq "doit") {
     );
         
     if ($res_id) {
-        print "Inserted resource $res_id : $name\n";
+        print "Inserted resource id $res_id : $full_res_name\n-------------\n";
     } else {
         die "Insert resource query error: ".Dumper $dbq->get_error();
     }
 
-   # insert event (will record current timestamp) (user can be null, but let's keep it for now)
+    # insert event (will record current timestamp) (user can be null, but let's keep it for now)
     my $event_id = $dbq->insert(
         table => 'event',
         fields => { user => $script_user_id,
@@ -451,6 +494,18 @@ elsif ($mode eq "doit") {
     if (!$event_id) {
         die "Insert event query error: ".Dumper $dbq->get_error();
     }
+
+   # Link project
+   my $link_id = $dbq->insert(
+        table => 'ip_block_project',
+        fields => { ip_block_id => $res_id,
+                    project_id => $proj_id
+                  }
+   );
+    if (!$link_id) {
+        die "Link project query error: ".Dumper $dbq->get_error();
+    }
+
 
 } # end if doit
 else { 
