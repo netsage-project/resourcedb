f82ea899053fa80bcabf9d6085872af27e05e65c
Lisa Ensman <lensman@globalnoc.iu.edu>
2022-10-14 14:44:37 +0000

    One more misc script

--- misc-scripts/query_mmdb.pl
+++ misc-scripts/query_mmdb.pl
@@ -1,14 +1,10 @@
 #!/usr/bin/env perl
-
-# This script can be used to view the info for an IP in the science registry mmdb file
-# (or any mmdb file with appropriate edits)
-# User must provide an mmdb filename/path and an IP address on the command line
-# RUN MANUALLY
-
-# based on https://blog.maxmind.com/2015/09/29/building-your-own-mmdb-database-for-fun-and-profit/
-
-# You can also get a tool here: https://github.com/maxmind/mmdbinspect
-
+#
+# This script can be used to view the info for an IP in any mmdb file (maxmind geoip db's, science registry fake geoip db)
+# Based on https://blog.maxmind.com/2015/09/29/building-your-own-mmdb-database-for-fun-and-profit/
+#
+# TO use:  perl query_mmdb.pl <mmdb filename> <ip_address>
+#
 use strict;
 use warnings;
 
@@ -18,15 +14,16 @@ use MaxMind::DB::Reader;
 my $file = shift @ARGV or die 'Usage: perl query_mmdb.pl <mmdb filename> <ip_address>';
 if (! -e $file) { die 'File $file does not exist\n'; }
 
-my $ip = shift @ARGV or die 'Usage: perl query_mmdb.pl <ip_address>';
+my $ip = shift @ARGV or die 'Usage: perl query_mmdb.pl <mmdb filename> <ip_address>';
 print "IP: $ip\n";
 
 my $reader = MaxMind::DB::Reader->new( file => $file );
 
 my $record = $reader->record_for_address( $ip );
 if ($record) {
-     print Dumper $record; # for any db
-    #print $record->{'city'}->{'names'}->{'en'}."\n"; # to see scireg data
-} else {
-    print "not found\n";
-}
+    # print $record->{'city'}->{'names'}->{'en'}."\n";     ## for science registry mmdb
+    print Dumper  $record;
+    print "\n";
+    } else {
+        print "not found\n";
+    }
